cbabb44976186434ec775748bc0e21cbb5287410
AgustinFuentes15 <agusfue21@gmail.com>
2024-04-24 11:08:16 -0300

    creacion y ruteo de modulo y componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './modules/inicio/pages/inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+//ruta comun -> 1 solo componente
+  {path:"",component:InicioComponent
+},
+//carga peresosa -> 1 solo componente
+//loadChildren indica una ruta hija
+//()=> ruta de donde vien el modulo 
+// then es una funcion asincronica que espera uns configuracion del exterior y lo que hace es "este bien o mal nos va a devolver algo"
+{path:"", loadChildren:()=>import('./modules/inicio/inicio.module').then(m=>m.InicioModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
