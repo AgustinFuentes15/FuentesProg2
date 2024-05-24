14f4e1212a97f5f827ec7afe4171d3314823bd42
AgustinFuentes15 <agusfue21@gmail.com>
2024-05-24 10:58:25 -0300

    creacion del modulo autentificacion-componentes registro e inicio sesion y manejo de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,17 +3,25 @@ import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './modules/inicio/pages/inicio/inicio.component';
 
 const routes: Routes = [
-//ruta comun -> 1 solo componente
-  {path:"",component:InicioComponent
-},
-//carga peresosa -> 1 solo componente
-//loadChildren indica una ruta hija
-//()=> ruta de donde vien el modulo 
-// then es una funcion asincronica que espera uns configuracion del exterior y lo que hace es "este bien o mal nos va a devolver algo"
-{path:"", loadChildren:()=>import('./modules/inicio/inicio.module').then(m=>m.InicioModule)
+  //ruta comun -> 1 solo componente
+  {
+    path: "", component: InicioComponent
   },
-  {path:"", loadChildren:()=>import('./modules/producto/producto.module').then(m=>m.ProductoModule)
-}
+  //carga peresosa -> 1 solo modulo
+  //loadChildren indica una ruta hija
+  //()=> ruta de donde vien el modulo 
+  // then es una funcion asincronica/promesa que espera uns configuracion del exterior y lo que hace es "este bien o mal nos va a devolver algo"
+  {
+    path: "", loadChildren: () => import('./modules/inicio/inicio.module').then(m => m.InicioModule)
+  },
+  {
+    path: "", loadChildren: () => import('./modules/producto/producto.module').then(m => m.ProductoModule)
+  },
+  {
+    path: "", loadChildren: () => import('./modules/autentificacion/autentificacion.module').then(m => m.AutentificacionModule)
+  }
+  
+
 
 ];
 
@@ -21,4 +29,6 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
+
 export class AppRoutingModule { }
---
+++ src/app/modules/autentificacion/autentificacion.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AutentificacionRoutingModule } from './autentificacion-routing.module';
+import { InicioSesionComponent } from './inicio-sesion/inicio-sesion.component';
+import { RegistroComponent } from './registro/registro.component';
+
+// componentes
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+
+@NgModule({
+  declarations: [
+    InicioSesionComponent,
+    RegistroComponent
+  ],
+  imports: [
+    CommonModule,
+    AutentificacionRoutingModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule
+  ],
+  //modulos locales se declaran y exportan
+  exports:[
+    RegistroComponent,
+    InicioSesionComponent,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule
+  ],
+})
+export class AutentificacionModule { }
