7f015ce5e5fe421327333ec568c1a35b24f58cee
AgustinFuentes15 <agusfue21@gmail.com>
2024-05-29 10:03:54 -0300

    solucionamos el error en el registro y creamos la interfaz usuario

---
+++ src/app/models/usuario.ts
@@ -0,0 +1,7 @@
+export interface Usuario {
+    uid:string| any; //Atributos tipo any = reciben valores indefinidos 
+    nombre: string;
+    apellido:string;
+    email:string;
+    password: string;
+}
